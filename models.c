eeddcd12115f5a486a3c47f53eceaff20f2b55cd
Rolf <rolf>
2020-05-21 00:21:11 -0300

    fixed regression by the changed a1 initial value of the SEIR model

--- models.c
+++ models.c
@@ -258,12 +258,12 @@ char *modelDescription_SEIR =
 int initialValues_SEIR(ldouble t1, ldouble min, ldouble max, ldouble A[mpar], int f[mpar])
 {
    if (isnan(A[0])) A[0] =  0.5L;                     // beta  - infection rate
-   if (isnan(A[1])) A[1] =  max;                      // VSP   - Virtual Susceptible Population
+   if (isnan(A[1])) A[1] =  2.0L*max;                 // VSP   - Virtual Susceptible Population
    if (isnan(A[3])) A[3] =  0.4L;                     // sigma - incubation rate  (2.5 d (latency) until an infected individual becomes infectious)
    if (isnan(A[4])) A[4] =  0.0625L;                  // gamma - removal rate (more 16 d until the infectious individual can be removed from the chain of infection)
    if (isnan(A[6])) A[6] =  min;                      // R(t1) boundary value at t1
    if (isnan(A[7])) A[7] =  t1;
-   if (isnan(A[8])) A[8] =  0.0;                      // d·TP  - diffusion constant x Total Population, for example 0.05*80e6 = 4000000
+   if (isnan(A[8])) A[8] =  0.0L;                     // d·TP  - diffusion constant x Total Population, for example 0.05*80e6 = 4000000
 
    if (isnan(A[2])) A[2] = A[6]/A[4]/A[3];            // total number of exposed individuals at t1
    if (isnan(A[5])) A[5] = (1.0L - A[4])*A[3]*A[2];   // I(t1) boundary value at t1
